5e1d9586084e6775c7fa9ac2095431388bf06ba5
shinbo91 <shinbo91@gmail.com>
2015-08-08 16:04:19 +0800

    Convert to Pelican

---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'shinbo91'
+SITENAME = u"shinbo91's blog"
+SITEURL = 'http://shinbo91.github.io'
+#SITESUBTITLE = u""
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = u'zh_TW'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# theme
+THEME = 'themes'
+
+# Disqus
+DISQUS_SITENAME = 'shinbo91blog'
+DISQUS_LOAD_LATER = True
+
+# GitHub
+GITHUB_URL = 'http://shinbo91.github.io'
